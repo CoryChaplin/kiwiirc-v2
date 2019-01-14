16aa8263898d24846573331651d471971af1122d
ItsOnlyBinary <github@itsonlybinary.com>
2019-01-14 15:57:33 +0000

    Allow sidebar in queries

--- src/components/ContainerHeader.vue
+++ src/components/ContainerHeader.vue
@@ -23,7 +23,7 @@
                 />
                 <div
                     :class="{
-                        'kiwi-header-option--active': sidebarState.sidebarSection === 'about'
+                        'kiwi-header-option--active': sidebarSection === 'about'
                     }"
                     class="kiwi-header-option kiwi-header-option-about"
                 >
@@ -33,8 +33,8 @@
                 </div>
                 <div
                     :class="{
-                        'kiwi-header-option--active': sidebarState.sidebarSection === 'nicklist'
-                            || sidebarState.sidebarSection === 'user'
+                        'kiwi-header-option--active': sidebarSection === 'nicklist'
+                            || sidebarSection === 'user'
                     }"
                     class="kiwi-header-option kiwi-header-option-nicklist"
                 >
@@ -48,7 +48,7 @@
                 </div>
                 <div
                     :class="{
-                        'kiwi-header-option--active': sidebarState.sidebarSection === 'settings'
+                        'kiwi-header-option--active': sidebarSection === 'settings'
                     }"
                     class="kiwi-header-option kiwi-header-option-settings"
                 >
@@ -121,6 +121,17 @@
             </div>
             <div :key="buffer.id" class="kiwi-header-options">
                 <div
+                    v-if="userOnline"
+                    :class="{
+                        'kiwi-header-option--active': sidebarSection === 'user'
+                    }"
+                    class="kiwi-header-option kiwi-header-option-user"
+                >
+                    <a @click="toggleUser()">
+                        <i class="fa fa-user" aria-hidden="true" />
+                    </a>
+                </div>
+                <div
                     v-for="plugin in pluginUiQueryElements"
                     :key="plugin.id"
                     v-rawElement="plugin.el"
@@ -178,7 +189,6 @@
 <script>
 'kiwi public';
 
-import state from '@/libs/state';
 import GlobalApi from '@/libs/GlobalApi';
 import toHtml from '@/libs/renderers/Html';
 import parseMessage from '@/libs/MessageParser';
@@ -195,7 +205,7 @@ export default {
         AwayStatusIndicator,
     },
     props: ['buffer', 'sidebarState'],
-    data: function data() {
+    data() {
         return {
             buffer_settings_open: false,
             pluginUiChannelElements: GlobalApi.singleton().channelHeaderPlugins,
@@ -206,14 +216,14 @@ export default {
         };
     },
     computed: {
-        isJoined: function isJoined() {
+        isJoined() {
             let buffer = this.buffer;
             return buffer.getNetwork().state === 'connected' && buffer.joined;
         },
-        isConnected: function isConnected() {
+        isConnected() {
             return this.buffer.getNetwork().state === 'connected';
         },
-        formattedTopic: function formattedTopic() {
+        formattedTopic() {
             let blocks = parseMessage(this.buffer.topic, { extras: false });
             let content = toHtml(blocks);
             return content;
@@ -221,15 +231,22 @@ export default {
         network() {
             return this.buffer.getNetwork();
         },
+        sidebarSection() {
+            return this.sidebarState.section();
+        },
+        userOnline() {
+            let user = this.$state.getUser(this.buffer.getNetwork().id, this.buffer.name);
+            return !!user;
+        },
     },
     watch: {
-        buffer: function watchBuffer() {
+        buffer() {
             // When ever the buffer changes, close the settings dropdown
             this.buffer_settings_open = false;
         },
     },
     created() {
-        this.listen(state, 'document.clicked', (e) => {
+        this.listen(this.$state, 'document.clicked', (e) => {
             // If clicking anywhere else on the page, close all our prompts
             if (!this.$el.contains(e.target)) {
                 Object.keys(this.prompts).forEach((prompt) => {
@@ -265,8 +282,9 @@ export default {
                 network.ircClient.connect();
             }
         },
-        showSidebar() {
-            state.$emit('sidebar.toggle');
+        toggleUser() {
+            let user = this.$state.getUser(this.buffer.getNetwork().id, this.buffer.name);
+            this.sidebarState.toggleUser(user);
         },
         joinCurrentBuffer() {
             let network = this.buffer.getNetwork();
@@ -274,13 +292,13 @@ export default {
             network.ircClient.join(this.buffer.name);
         },
         closeCurrentBuffer() {
-            state.removeBuffer(this.buffer);
+            this.$state.removeBuffer(this.buffer);
         },
         onHeaderClick(event) {
             let channelName = event.target.getAttribute('data-channel-name');
             if (channelName) {
                 let network = this.buffer.getNetwork();
-                state.addBuffer(this.buffer.networkid, channelName);
+                this.$state.addBuffer(this.buffer.networkid, channelName);
                 network.ircClient.join(channelName);
             }
         },
